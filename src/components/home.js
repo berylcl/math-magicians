e09225b7eae9a61f824180eaf920bd01e1bddc59
berylcl <gakiiberyl@gmail.com>
2023-05-31 16:51:28 +0100

    Added home and navbar to build full website

---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => (
+  <nav className="welcome-section">
+    <h2>Welcome to my page</h2>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Quas magni ab expedita natus dolor architecto, ducimus
+      reiciendis voluptate consequatur cumque, maiores iusto temporibus,
+      tempora reprehenderit autem. Officia culpa eligendi cumque.
+    </p>
+  </nav>
+);
+
+export default Home;
