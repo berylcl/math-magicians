9ae753b3876dd1cce10ff3fbb1f7267e1683f643
berylcl <gakiiberyl@gmail.com>
2023-05-31 15:34:38 +0100

    Implementing Quotes from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import Calculator from './components/calculator';
+import Quote from './components/quote';
 
 function App() {
-  return (<Calculator />);
+  return (
+    <div>
+      <Calculator />
+      <Quote />
+    </div>
+  );
 }
 
 export default App;
